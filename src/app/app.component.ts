99bab63e5e9ee37abb7cacdcdf0ee383908d598c
shusson <shane.a.husson@gmail.com>
2018-10-30 14:28:29 +0100

    add use case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subject, Observable } from "rxjs";
+import { startWith, map, debounceTime } from "rxjs/operators";
+
+const DEFAULT_RESULTS = ["a", "b", "c"];
+const SEARCH_RESULTS = ["d", "e", "f"];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'test-ng-select';
+export class AppComponent implements OnInit {
+    title = "test-ng-select";
+    autocompleteInput$ = new Subject<string>();
+    items = [];
+    filter = {
+        values: []
+    };
+    search$: Observable<any[]>;
+
+    constructor() {}
+
+    ngOnInit() {
+        this.search$ = this.autocompleteInput$.pipe(
+            startWith(""),
+            debounceTime(300),
+            map((term) => {
+                const searchTerm = term && term.length > 0 ? term : "";
+
+                console.log(term);
+
+                if (!searchTerm) {
+                    return DEFAULT_RESULTS;
+                } else {
+                    return SEARCH_RESULTS;
+                }
+            })
+        );
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgSelectModule } from "@ng-select/ng-select";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, NgSelectModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
